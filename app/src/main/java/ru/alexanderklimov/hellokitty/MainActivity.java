07c89b7ac7072e532b2385f0c6e31da01e1b8df4
Alexander Klimoff <alexander.klimoff@gmail.com>
2016-09-23 17:51:39 +0300

    Android Studio 2.2

---
+++ app/src/main/java/ru/alexanderklimov/hellokitty/MainActivity.java
@@ -0,0 +1,30 @@
+package ru.alexanderklimov.hellokitty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mHelloTextView;
+    private EditText mNameEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mHelloTextView = (TextView) findViewById(R.id.textView);
+        mNameEditText = (EditText) findViewById(R.id.editText);
+    }
+
+    public void onClick(View view) {
+        if (mNameEditText.getText().length() == 0) {
+            mHelloTextView.setText("Hello Kitty!");
+        } else {
+            mHelloTextView.setText("Привет, " + mNameEditText.getText());
+        }
+    }
+}
